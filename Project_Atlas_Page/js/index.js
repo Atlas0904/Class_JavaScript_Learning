196ae39cf4923a897234b9eb9fcdeda5a4757ed4
atlas0904 <u920243@gmail.com>
2016-04-02 19:24:20 +0800

    Change color for nav

--- Project_Atlas_Page/js/index.js
+++ Project_Atlas_Page/js/index.js
@@ -1,5 +1,19 @@
 function showContent(choice) {
-	if (choice=="Introduction") {
-		
+	// 先把大家都藏起來
+	var page;
+	for(var i=0; i< nav_names.length; i++) {
+		page = document.getElementById("page-" + i);
+		page.style.display="none";
+	}
+	page = document.getElementById("page-" + choice);
+	page.style.display = "block";
+}
+function changeNavColor(elt, action) {
+	if (action == "over") {
+		elt.style.color = "#2F4F4F";
+	} else if (action == "out") {
+		elt.style.color = "white";
+	} else {
+		elt.style.color = "white";
 	}
 }
