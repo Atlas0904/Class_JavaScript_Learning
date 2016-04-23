37c9f3a4794810d7dc111e6c3d8765d790d18df1
atlas0904 <u920243@gmail.com>
2016-04-24 00:43:17 +0800

    Switch JavaScript Adv to JavaScript_Adv

--- AtlasLib/JS/fade.js
+++ AtlasLib/JS/fade.js
@@ -4,6 +4,29 @@ var idFaceIn;
 var idFaceOut;
 var opacity = 0.5;
 
+function init(){  // body 前
+	for(i=1; i<=2; i++) {
+		box = document.getElementById("box-"+i);  // Global var 方便用
+		box.addEventListener("mousedown", mousedown);
+		box.addEventListener("mouseup", mouseup);
+	}
+}
+
+function mousedown(e) {
+	delta_x = e.clientX - box.offsetLeft;
+	delta_y =  e.clientY - box.offsetTop;
+	document.addEventListener("mousemove", move);
+}
+function mouseup() {
+	document.removeEventListener("mousemove", move);
+}
+
+// Need to refine, how to know which box you selected
+function move(e){
+	box.style.left=(e.clientX - delta_x)  + "px";  // note 0px 不行
+	box.style.top= (e.clientY - delta_y) + "px";
+}
+
 function faceIn(elt) {
 	elt.innerHTML = "Mouse in."
 	window.clearInterval(idFaceOut);
